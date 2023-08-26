8d31951ed4b533389c781af058f4be7c20450408
Barahalikar Siddharth <barahalikar.siddharth@gmail.com>
2023-08-26 23:20:11 +0530

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+const { Octokit } = require('@octokit/rest');
+const GphApiClient = require('giphy-js-sdk-core');
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    
+    const githubToken = core.getInput('github-token');
+    const giphyApiKey = core.getInput('giphy-api-key');
+
+    const octokit = new Octokit({ auth: githubToken });
+    const giphy = GphApiClient(giphyApiKey);
+
+    const context = github.context;
+    const { owner, repo, number } = context.issue;
+    const prComment = await giphy.random('funny');
+
+    const commentResponse = await octokit.issues.createComment({
+      owner,
+      repo,
+      issue_number: number,
+      body: `![Giphy]( ${prComment.data.images.downsized.url} )`
+    });
+
+    const commentUrl = commentResponse.data.images.downsized.url;
+    core.setOutput('comment-url', commentUrl);
+
+    console.log(`Giphy GIF comment added successfully! Comment URL: ${commentUrl}`);
+  } catch (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+}
+
+run();
